44e8d9fef7d8782f3524e0399eac50341fbe3d70
babool.wasule <babool.wasule@gmail.com>
2018-03-10 22:36:18 +0530

    shopping cart

---
+++ my-new-shop1/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDbIIJ3PTibXNxa6Z0kZHlcxhzZkTj49_Q",
+   authDomain: "my-new-shop.firebaseapp.com",
+   databaseURL: "https://my-new-shop.firebaseio.com",
+   projectId: "my-new-shop",
+   storageBucket: "my-new-shop.appspot.com",
+   messagingSenderId: "140547924351"
+  }
+};
